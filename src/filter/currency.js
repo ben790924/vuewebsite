50db7e78240143857ee67e628522dee38589fc7a
BEN <37503754+ben790924@users.noreply.github.com>
2022-12-22 15:38:09 +0800

    Update currency.js

--- src/filter/currency.js
+++ src/filter/currency.js
@@ -1,4 +1,5 @@
 export default function (num) {
+    // test wenhook
     const n = Number(num);
     return `$${n.toFixed(0).replace(/./g, (c, i, a) => {
         const currency = (i && c !== '.' && ((a.length - i) % 3 === 0) ? `, ${c}`.replace(/\s/g, '') : c);
